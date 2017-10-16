bef7ee2429da97bb6b33d82bfa10b5948222a9e9
prexsa <prexgit@gmail.com>
2017-10-16 15:18:15 -0700

    add create-react-app as front boilerplate

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 
+app.use(bodyParser.json());
+app.use(cors);
 app.get('/', (req, res) => {
   res.send({ hi: 'there' });
 });
 
-const PORT = process.env.PORT || 3090;
 
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 3090;
 app.listen(PORT);
